3238993642f6f7519235df81cc408f73a47b5e36
Doug McPeek <dmcpeek@outlook.com>
2023-04-28 16:59:21 -0600

    First check in

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> myPassportStamps = new List<string> { "Germany", "Holland", "Belgium", "England", "Egypt", "Tanzania", "Costa Rica", "China", "Cambodia", "Thailand", "Nepal", "India", "Israel", "Jordan", "Italy", "Peru", "Chili" };
+
+
+
+            var countries = myPassportStamps.OrderBy(x => x.Length);
+
+            Console.WriteLine("The Places I've Travelled To");
+            Console.WriteLine("----------------------------");
+            foreach (var country in countries) 
+            {
+                Console.WriteLine(country);
+            }
+            //Console.WriteLine("Hello, World!");
         }
     }
 }
